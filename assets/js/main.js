598a50089c4eb4d7361b80dea2cd3c631b9542f3
Benjamin Leuckefeld <code@deratrox.de>
2016-11-07 23:45:44 +0100

    added base dA implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,11 @@
 $(function () {
     $('[data-toggle="tooltip"]').tooltip()
-})
+});
 
 $('#locales a').on('click', function () {
     Cookies.set('lang', $(this).html());
     location.reload();
-})
+});
 
 $('#dismiss').on('click', function () {
     $(this).parent().parent().slideUp(function () {
@@ -14,3 +14,59 @@ $('#dismiss').on('click', function () {
 
     Cookies.set('hide_jumbo', 1);
 });
+
+$('#getImage').on('click', function () {
+    var url = $('#url').val();
+
+    $(this).prop('disabled', true);
+    $('#url').prop('disabled', true);
+    $('#loading').slideDown();
+
+    $.getJSON('http://backend.deviantart.com/oembed?url=' + url + '&format=jsonp&callback=?')
+        .done(function (data) {
+            addImage(data);
+        })
+        .fail(function (data) {
+            console.log(data);
+            $('#failed').slideDown();
+            setTimeout(function () {
+                $('#failed').slideUp();
+            }, 3000);
+        })
+        .always(function () {
+            $('#getImage').prop('disabled', false);
+            $('#url').prop('disabled', false);
+            $('#loading').slideUp();
+        });
+
+    /*$.post('ajax/GetImage.php', {url: url})
+        .done(function (data) {
+            addImage(data);
+        })
+        .fail(function (data) {
+            console.log(data);
+            $('#failed').slideDown();
+            setTimeout(function () {
+                $('#failed').slideUp();
+            }, 3000);
+        })
+        .always(function () {
+            $('#getImage').prop('disabled', false);
+            $('#url').prop('disabled', false);
+            $('#loading').slideUp();
+        });*/
+});
+
+$('#wrapper').on('click', '.remove',function () {
+    $(this).parent().parent().slideUp(function () {
+        $(this).remove();
+    });
+});
+
+function addImage(data){
+    $.get('views/elements/image.html', function (template) {
+        var rendered = Mustache.render(template, data);
+        $(rendered).appendTo($('#wrapper')).hide().slideDown();
+
+    });
+}
