cd586579d3dff3c47db33b97046a011f63b84370
Benjamin Leuckefeld <code@deratrox.de>
2016-11-08 17:16:52 +0100

    main oembed implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -23,41 +23,74 @@ $('#getImage').on('click', function () {
     $('#url').prop('disabled', true);
     $('#loading').slideDown();
 
+    if(url.includes('deviantart.com') || url.includes('fav.me') || url.includes('sta.sh')) {
+        getDeviantart(url, addImage);
+    } else if(url.includes('derpibooru.org')) {
+        getDerpibooru(url, addImage);
+    }
+
+});
+
+function getDeviantart(url, callback) {
+    var result = null;
+
+    $.getJSON('http://backend.deviantart.com/oembed?url=' + url + '&format=jsonp&callback=?')
+        .done(function (data) {
+            if(!('url' in data)) {
+                data.url = data.thumbnail_url;
+            }
+
+            result = data;
+        })
+        .fail(function (data) {
+            console.log(data);
+        })
+        .always(function () {
+            callback(result);
+        });
+}
+
+function getDerpibooru(url, callback) {
+    var result = null;
+    var added = false;
+
     $.getJSON('https://derpibooru.org/oembed.json?url=' + url)
         .done(function (data) {
             data.url = data.thumbnail_url;
-            addImage(data);
+            result = data;
+
+            if(data.author_url.includes('deviantart.com')) {
+                getDeviantart(data.author_url, addImage);
+                added = true;
+            }
         })
         .fail(function (data) {
             console.log(data);
-            $('#failed').slideDown();
-            setTimeout(function () {
-                $('#failed').slideUp();
-            }, 3000);
         })
         .always(function () {
-            $('#getImage').prop('disabled', false);
-            $('#url').prop('disabled', false);
-            $('#loading').slideUp();
+            if(!added) callback(result);
         });
 
-    /*$.getJSON('http://backend.deviantart.com/oembed?url=' + url + '&format=jsonp&callback=?')
-        .done(function (data) {
-            addImage(data);
-        })
-        .fail(function (data) {
-            console.log(data);
-            $('#failed').slideDown();
-            setTimeout(function () {
-                $('#failed').slideUp();
-            }, 3000);
-        })
-        .always(function () {
-            $('#getImage').prop('disabled', false);
-            $('#url').prop('disabled', false);
-            $('#loading').slideUp();
-        });*/
-});
+}
+
+function addImage(data){
+    if(data == null || 'error' in data) {
+        $('#failed').slideDown();
+        setTimeout(function () {
+            $('#failed').slideUp();
+        }, 3000);
+    } else {
+        $.get('views/elements/image.html', function (template) {
+            var rendered = Mustache.render(template, data);
+            $(rendered).appendTo($('#wrapper')).hide().slideDown();
+
+        });
+    }
+
+    $('#getImage').prop('disabled', false);
+    $('#url').prop('disabled', false);
+    $('#loading').slideUp();
+}
 
 $('#wrapper').on('click', '.remove',function () {
     $(this).parent().parent().parent().slideUp(function () {
@@ -65,10 +98,3 @@ $('#wrapper').on('click', '.remove',function () {
     });
 });
 
-function addImage(data){
-    $.get('views/elements/image.html', function (template) {
-        var rendered = Mustache.render(template, data);
-        $(rendered).appendTo($('#wrapper')).hide().slideDown();
-
-    });
-}
